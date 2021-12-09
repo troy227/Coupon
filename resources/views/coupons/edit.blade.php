ad83486a1057c2cd8b237671a069f8f750c5bd7b
troy227 <tushar.roy@niyotail.com>
2021-12-09 15:27:54 +0530

    Bug fixes

--- resources/views/coupons/edit.blade.php
+++ resources/views/coupons/edit.blade.php
@@ -5,10 +5,7 @@
     <h1 class="text-center my-5">Edit Coupon</h1>
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card card-default">
-                <div class="card-header">
-                    Edit Coupon
-                </div>
+
                 <div class="card-body">
                     @if ($errors->any())
                         <div class="alert alert-danger">
@@ -31,20 +28,41 @@
 
                                 <input type="text" class="form-control" name="name" placeholder="Coupon Name"
                                 value="{{$coupon->coupon_name}}">
+                                <div class="card-header">
+                                    Coupon Code
+                                </div>
                                 <input type="text" class="form-control" name="code" placeholder="Coupon Code"
                                        value="{{$coupon->coupon_code}}">
+                                <div class="card-header">
+                                    Description
+                                </div>
                                 <input type="text" class="form-control" name="desc" placeholder="Description"
                                        value="{{$coupon->description}}">
+                                <div class="card-header">
+                                    Valid From
+                                </div>
                                 <input type="text" placeholder="Valid From"
                                        onfocus="(this.type='date')" class="form-control" name="valid_from"
                                        value="{{$coupon->valid_from}}">
+                                <div class="card-header">
+                                    Valid Until
+                                </div>
                                 <input type="text" placeholder="Valid Until"
                                        onfocus="(this.type='date')" class="form-control" name="valid_until"
                                        value="{{$coupon->valid_until}}">
+                                <div class="card-header">
+                                    Coupon Amount
+                                </div>
                                 <input type="text" class="form-control" name="amount" placeholder="Coupon Amount"
                                        value="{{$coupon->coupon_amount}}">
+                                <div class="card-header">
+                                    Max Redeems
+                                </div>
                                 <input type="text" class="form-control" name="max_redeem" placeholder="Max Redeem"
                                        value="{{$coupon->max_redeem}}">
+                                <div class="card-header">
+                                    Max Redeem per User
+                                </div>
                                 <input type="text" class="form-control" name="redeem_per_user" placeholder="Max Redeem Per User"
                                        value="{{$coupon->max_redeem_per_user}}">
                             <div class="form-group text-center">
@@ -54,7 +72,7 @@
                             </div>
                         </form>
                 </div>
-            </div>
+
         </div>
     </div>
 
