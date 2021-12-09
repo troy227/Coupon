e9cc8971fca8bf3aa321f29d12a1366cab95b036
troy227 <tushar.roy@niyotail.com>
2021-12-09 12:29:00 +0530

    added show all coupons

--- database/migrations/2021_12_09_063209_create_coupon_table.php
+++ database/migrations/2021_12_09_063209_create_coupon_table.php
@@ -13,7 +13,7 @@ class CreateCouponTable extends Migration
      */
     public function up()
     {
-        Schema::create('coupon', function (Blueprint $table) {
+        Schema::create('coupons', function (Blueprint $table) {
             $table->increments('id');
             $table->string('coupon_name');
             $table->string('coupon_code');
